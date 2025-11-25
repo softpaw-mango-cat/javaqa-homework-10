4878b179053278e254b56b7d29bf5cf42a236312
katja-b <katerina-balakireva@yandex.ru>
2025-11-25 13:58:34 +0300

    fixed manager and tests

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -1,8 +1,8 @@
 package ru.netology.manager;
 
 public class MovieManager {
-   private MovieItem[] items;
-   private int movieLimit;
+    private MovieItem[] items;
+    private int movieLimit;
 
     public MovieManager() {
         this.items = new MovieItem[0];
@@ -24,24 +24,30 @@ public class MovieManager {
     }
 
     public void addItem(MovieItem item) {
-        MovieItem[] tmp = new MovieItem[items.length+1];
+        MovieItem[] tmp = new MovieItem[items.length + 1];
         for (int i = 0; i < items.length; i++) {
             tmp[i] = items[i];
         }
-        tmp[tmp.length-1] = item;
+        tmp[tmp.length - 1] = item;
         items = tmp;
-   }
+    }
 
-   public MovieItem[] findAll() {
-       return items;
-   }
+    public MovieItem[] findAll() {
+        return items;
+    }
 
-   public MovieItem[] findLast() {
+    public MovieItem[] findLast() {
+        int resultLength;
+        if (items.length <= movieLimit) {
+            resultLength = items.length;
+        } else {
+            resultLength = movieLimit;
+        }
 
-       MovieItem[] reversed = new MovieItem[movieLimit];
-       for (int i = 0; i < reversed.length; i++) {
-           reversed[i] = items[items.length - 1 - i];
-       }
-       return reversed;
-   }
+        MovieItem[] reversed = new MovieItem[resultLength];
+        for (int i = 0; i < reversed.length; i++) {
+            reversed[i] = items[items.length - 1 - i];
+        }
+        return reversed;
+    }
 }
--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -10,9 +10,7 @@ public class MovieManagerTest {
     MovieItem item3;
     MovieItem item4;
     MovieItem item5;
-
-    MovieManager testManager;
-    MovieManager testManagerWithLimit;
+    MovieItem item6;
 
     @BeforeEach
     public void addData() {
@@ -21,70 +19,177 @@ public class MovieManagerTest {
         item3 = new MovieItem("Отель Белград", "комедия", 2023, "6+");
         item4 = new MovieItem("Джентельмены", "боевик", 2024, "18+");
         item5 = new MovieItem("Человек-невидимка", "ужасы", 2025, "18+");
+        item6 = new MovieItem("Номер один", "комедия", 2024, "12+");
+    }
 
-        testManager = new MovieManager();
-        testManagerWithLimit = new MovieManager(3);
+    // проверки для addItem
+    @Test
+    public void shouldAddNewItemWhenEmpty() {
+        MovieManager manager = new MovieManager();
+        manager.addItem(item1);
+
+        MovieItem[] expected = {item1};
+        MovieItem[] actual = manager.getItems();
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void shouldAddNewItemWhenNotEmpty() {
         MovieItem[] items = {item1, item2, item3, item4, item5};
-        testManager.setItems(items);
+        MovieManager manager = new MovieManager();
+        manager.setItems(items);
 
-        MovieItem item6 = new MovieItem(
-                "Номер один",
-                "комедия",
-                2024,
-                "12+");
+        manager.addItem(item6);
 
-        testManager.addItem(item6);
         MovieItem[] expected = {item1, item2, item3, item4, item5, item6};
-        MovieItem[] actual = testManager.getItems();
+        MovieItem[] actual = manager.getItems();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    // проверки для findAll
+    @Test
+    public void shouldFindAllMoviesWhenEmpty() {
+        MovieManager manager = new MovieManager();
+        MovieItem[] expected = {};
+        MovieItem[] actual = manager.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldAddNewItemWhenEmpty() {
-        MovieItem[] expected = {item1};
-        testManager.addItem(item1);
-        MovieItem[] actual = testManager.getItems();
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldFindLastAddedDefaultLimit() {
+    public void shouldFindAllMoviesWhenNotEmpty() {
+        MovieManager manager = new MovieManager();
         MovieItem[] items = {item1, item2, item3, item4, item5};
-        testManager.setItems(items);
-
-        MovieItem[] expected = {item5, item4, item3, item2, item1};
-        MovieItem[] actual = testManager.findLast();
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldFindLastAddedSetLimit() {
-        MovieItem[] items = {item1, item2, item3, item4, item5};
-        testManagerWithLimit.setItems(items);
-
-        MovieItem[] expected = {item5, item4, item3};
-        MovieItem[] actual = testManagerWithLimit.findLast();
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldFindAllMovies() {
-
-        MovieItem[] items = {item1, item2, item3, item4, item5};
-        testManager.setItems(items);
+        manager.setItems(items);
 
         MovieItem[] expected = {item1, item2, item3, item4, item5};
-        MovieItem[] actual = testManager.findAll();
+        MovieItem[] actual = manager.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
+    }
 
+    // проверки для findLast
+    @Test
+    public void shouldFindLastDefaultLimitEquals() {
+        // когда кол-во фильмов и лимит равны при дефолтном лимите
+        MovieManager manager = new MovieManager();
+        MovieItem[] items = {item1, item2, item3, item4, item5};
+        manager.setItems(items);
+
+        MovieItem[] expected = {item5, item4, item3, item2, item1};
+        MovieItem[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastDefaultLimitLow() {
+        // когда кол-во фильмов меньше лимита при дефолтном лимите
+        MovieManager manager = new MovieManager();
+        MovieItem[] items = {item1, item2};
+        manager.setItems(items);
+
+        MovieItem[] expected = {item2, item1};
+        MovieItem[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastDefaultLimitHigh() {
+        // когда кол-во фильмов больше лимита при дефолтном лимите
+        MovieManager manager = new MovieManager();
+        MovieItem[] items = {item1, item2, item3, item4, item5, item6};
+        manager.setItems(items);
+
+        MovieItem[] expected = {item6, item5, item4, item3, item2};
+        MovieItem[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastLowerLimitEquals() {
+        // когда кол-во фильмов и лимит равны при лимите меньше дефолтного
+        MovieManager manager = new MovieManager(3);
+        MovieItem[] items = {item1, item2, item3};
+        manager.setItems(items);
+
+        MovieItem[] expected = {item3, item2, item1};
+        MovieItem[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastLowerLimitLow() {
+        // когда кол-во фильмов меньше лимита при лимите меньше дефолтного
+        MovieManager manager = new MovieManager(3);
+        MovieItem[] items = {item1, item2};
+        manager.setItems(items);
+
+        MovieItem[] expected = {item2, item1};
+        MovieItem[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastLowerLimitHigh() {
+        // когда кол-во фильмов больше лимита при лимите меньше дефолтного
+        MovieManager manager = new MovieManager(3);
+        MovieItem[] items = {item1, item2, item3, item4, item5, item6};
+        manager.setItems(items);
+
+        MovieItem[] expected = {item6, item5, item4};
+        MovieItem[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastHigherLimitEquals() {
+        // когда кол-во фильмов и лимит равны при лимите больше дефолтного
+        MovieManager manager = new MovieManager(6);
+        MovieItem[] items = {item1, item2, item3, item4, item5, item6};
+        manager.setItems(items);
+
+        MovieItem[] expected = {item6, item5, item4, item3, item2, item1};
+        MovieItem[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastHigherLimitLow() {
+        // когда кол-во фильмов меньше лимита при лимите больше дефолтного
+        MovieManager manager = new MovieManager(6);
+        MovieItem[] items = {item1, item2};
+        manager.setItems(items);
+
+        MovieItem[] expected = {item2, item1};
+        MovieItem[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastHigherLimitHigh() {
+        // когда кол-во фильмов больше лимита при лимите больше дефолтного
+        MovieManager manager = new MovieManager(6);
+        MovieItem item7 = new MovieItem(
+                "Тролли",
+                "мультфильм",
+                2020,
+                "6+");
+        MovieItem[] items = {item1, item2, item3, item4, item5, item6, item7};
+        manager.setItems(items);
+
+        MovieItem[] expected = {item7, item6, item5, item4, item3, item2};
+        MovieItem[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
